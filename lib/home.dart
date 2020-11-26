330643ce8882500b8a1eb08c187d6a7d29661d7f
PythonPhil-Crack <phil.black@gmx.net>
2020-11-26 22:35:10 +0100

    tracked all file

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'graph_response.dart';
 import 'dart:convert';
-//import 'landing_page.dart';
+import 'package:flutter/cupertino.dart';
 
 
 class Home extends StatefulWidget {
@@ -23,109 +22,154 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
-      appBar: AppBar(
-        title: Text("Provisions Rechner"),
-        backgroundColor: Colors.grey[900],
-      ),
+      backgroundColor: Color(0XFF256075),
       body: SafeArea(
-        child: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
+        child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.bottomLeft,
+            child: IconButton(
+              onPressed: (){Navigator.pop(context,'/home');},
+              icon: Icon(CupertinoIcons.arrowtriangle_left_fill),
               color: Colors.white,
-            ),
-            margin: EdgeInsets.fromLTRB(50, 50, 50, 100),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Geburtstag",
-                    ),
-                    controller: gb_controller,
+              //label: Text(""),
+              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0,70.0,18.0,20),
+            child: Container(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                  "Eingabe",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.w800
                   ),
-                  SizedBox(height: 15),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Brutto-Verdienst",
-                    ),
-                    controller: brutto_controller,
-                  ),
-                  SizedBox(height: 15),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "bAV-Beitrag",
-                    ),
-                    controller: bAV_controller,
-                  ),
-                  SizedBox(height: 15),
-                  Container(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    //color: Colors.amber,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[400], //                   <--- border color
-                        width: 1.0,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: DropdownButton<String>(
-                        hint: Text(selectedValue),
-                        iconSize: 20,
-                        items: steuerklassen.map((String dropDownStringItem) {
-                          return DropdownMenuItem(
-                            value:dropDownStringItem,
-                            child: Text(dropDownStringItem),
-
-                          );
-                        }).toList(),
-                        onChanged: (selectedValue){
-                          setState(() {
-                            for (int i = 1; i <= steuerklassen.length; i++)
-                              if (steuerklassen[i] == selectedValue) {
-                                this.steuerklassen_value = i + 1;
-                              }
-                            //value: selectedValue;
-                            return steuerklassen_value;
-                          });
-                        },
-                      ),
-                    ),
-                  )
-                ],
               ),
             ),
           ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50.0)
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                //margin: EdgeInsets.only(top:80),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30,50,40,0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Geburtstag",
+                        ),
+                        controller: gb_controller,
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Brutto-Verdienst",
+                        ),
+                        controller: brutto_controller,
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "bAV-Beitrag",
+                        ),
+                        controller: bAV_controller,
+                      ),
+                      SizedBox(height: 15),
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width,
+                        //color: Colors.amber,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey[400], //                   <--- border color
+                            width: 1.0,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: DropdownButton<String>(
+                            hint: Text(selectedValue),
+                            iconSize: 20,
+                            items: steuerklassen.map((String dropDownStringItem) {
+                              return DropdownMenuItem(
+                                value:dropDownStringItem,
+                                child: Text(dropDownStringItem),
+
+                              );
+                            }).toList(),
+                            onChanged: (selectedValue){
+                              setState(() {
+                                for (int i = 1; i <= steuerklassen.length; i++)
+                                  if (steuerklassen[i] == selectedValue) {
+                                    this.steuerklassen_value = i + 1;
+                                  }
+                                //value: selectedValue;
+                                return steuerklassen_value;
+                              });
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+         // ),
+          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FlatButton.icon(
-        icon: Icon(Icons.calculate),
-        shape:
-        RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-        color: Colors.white,
-        label: Text("Provision errechnen"),
-        onPressed: () async {
-          AlertDialog(
-            title: Text("Bitte alle Felder eingeben"),
-          );
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.fromLTRB(50,0,50,15),
+        child: Container(
+          height: 50,
+          width: MediaQuery.of(context).size.width,
+          child: FlatButton.icon(
+            icon: Icon(
+                Icons.calculate,
+                color: Colors.white,
+            ),
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            color: Color(0XFF256075),
+            label: Text(
+                "Rechnen",
+                style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 0.5
+                ),
+            ),
+            onPressed: () async {
+              AlertDialog(
+                title: Text("Bitte alle Felder eingeben"),
+              );
 
-          final CalculatedGraph graph = await createUserData(gb_controller.text,
-              brutto_controller.text, bAV_controller.text, steuerklassen_value);
+              final CalculatedGraph graph = await createUserData(gb_controller.text,
+                  brutto_controller.text, bAV_controller.text, steuerklassen_value);
 
-          //json_response = get_image();
+              //json_response = get_image();
 
-          Navigator.pushNamed(context, '/calculatedAnswer');
-        },
+              Navigator.pushNamed(context, '/calculatedAnswer');
+            },
+          ),
+        ),
       ),
     );
   }
